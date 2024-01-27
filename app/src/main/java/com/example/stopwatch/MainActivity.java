1a60b59efb36d72ac88d273604c984af539284c6
ALIHDMI <alibek.shakirov2002@mail.ru>
2024-01-27 15:44:50 +0300

    first commit

---
+++ app/src/main/java/com/example/stopwatch/MainActivity.java
@@ -0,0 +1,61 @@
+package com.example.stopwatch;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.Locale;
+
+public class MainActivity extends AppCompatActivity {
+    private int seconds = 0;
+    private boolean running;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        if (savedInstanceState != null) {
+            seconds = savedInstanceState.getInt("seconds");
+            running = savedInstanceState.getBoolean("running");
+        }
+        runTimer();
+    }
+
+    public void onClickStart(View view) {
+        running = true;
+    }
+    public void onClickStop(View view) {
+        running = false;
+    }
+    public void onClickReset(View view) {
+        running = false;
+        seconds = 0;
+    }
+    @Override
+    public void onSaveInstanceState(Bundle instance) {
+        super.onSaveInstanceState(instance);
+        instance.putInt("seconds", seconds);
+        instance.putBoolean("running", running);
+    }
+    private void runTimer() {
+        final TextView timeView = findViewById(R.id.textView);
+        final Handler handler = new Handler();
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                int hours = seconds / 3600;
+                int minuts = (seconds % 3600) / 60;
+                int secs = seconds % 60;
+                String time = String.format(Locale.getDefault(), "%d:%02d:%02d", hours, minuts, secs);
+                timeView.setText(time);
+                if (running) {
+                    seconds++;
+                }
+                handler.postDelayed(this, 1000);
+            }
+        });
+
+    }
+}
